344557538e5f32bc6ef11e3a97e95c9843399fda
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-11-01 16:58:29 +0200

    fixed docker error

--- backend/order_service/src/index.js
+++ backend/order_service/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 6000;
+const port = 5050;
 
 // Define a route that responds with 'Hello, World!'
 app.get('/', (req, res) => {
