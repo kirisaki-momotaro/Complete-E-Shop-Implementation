4847d335e6fc525b0f5656fdcb6bcdeeebc1c030
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-12-13 14:43:28 +0200

    implemented login

--- frontend/src/src/index.js
+++ frontend/src/src/index.js
@@ -15,6 +15,60 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+function decodeJwt(jwtToken) {
+    const base64Url = jwtToken.split('.')[1]; // Get the payload part of the JWT
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace Base64 URL encoding characters
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join('')); // Decode Base64 and handle URI component encoding
+  
+    return JSON.parse(jsonPayload);
+  }
+
+async function Login(e){
+    e.preventDefault();
+
+    const login_username=document.getElementById('login-username').value;
+    const login_password=document.getElementById('login-password').value;
+    try{
+        console.log(login_username)
+        console.log(login_password);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("username", login_username);
+        urlencoded.append("password", login_password);
+        urlencoded.append("client_id", "client-front");
+        urlencoded.append("client_secret", "XC5qe0WjNIh9XIT0UwU4urbSmcBKo8ft");
+        urlencoded.append("grant_type", "password");
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: urlencoded,
+            redirect: 'follow'
+        };
+
+       const response= await fetch("http://localhost:8080/auth/realms/hmmyzon/protocol/openid-connect/token", requestOptions)
+        if(response.ok)
+        {
+            const login=await response.json();
+            const token=login.access_token
+
+            const decodeToken =await decodeJwt(token);
+            console.log(decodeToken)
+            
+        }else{
+            const err = await response.json();
+            console.log(err);
+        }
+
+    }catch(error) {
+        console.log(error);
+    }
+    return false
+}
 
 async function Register(e){
 
