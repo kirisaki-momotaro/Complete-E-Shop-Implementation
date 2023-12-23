19ebcf7b23d7d2f0154efbbdd7603419bb6f674c
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-12-23 05:52:01 +0200

    kafka issues

---
+++ backend/product_service/kafka.js
@@ -0,0 +1,32 @@
+const { Kafka, Partitioners } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'products-app',
+  brokers: ['kafka:8097'],
+  retry: {
+    initialRetryTime: 2000,
+    retries: 5
+  }
+})
+
+const producer = kafka.producer({
+    allowAutoTopicCreation: true,
+    createPartitioner: Partitioners.LegacyPartitioner
+})
+
+const sendOrders = async (msg)=>{
+ await producer.connect()
+ await producer.send({
+    topic: 'productsProducer',
+    messages: [{
+        value: JSON.stringify(msg)
+    }]
+ })
+
+ await producer.disconnect()
+}
+
+const consumer = kafka.consumer({
+    groupId: "products_group",
+    allowAutoTopicCreation: true,
+})
