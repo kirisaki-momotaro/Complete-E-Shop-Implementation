49c7facdfe47671160cdd6b7939a6a764a70e390
christos ioannidis <74102218+kirisaki-momotaro@users.noreply.github.com>
2023-12-27 18:38:01 +0200

    fixed products connection to kafka

--- backend/product_service/kafka.js
+++ backend/product_service/kafka.js
@@ -2,7 +2,7 @@ const { Kafka, Partitioners } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'products-app',
-  brokers: ['kafka:8097'],
+  brokers: ['kafka:19092'],
   retry: {
     initialRetryTime: 2000,
     retries: 5
